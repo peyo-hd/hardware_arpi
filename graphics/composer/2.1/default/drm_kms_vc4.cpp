ddfe7884866beed44e0fa664d6c465b3d13a5061
Peter Yoon <peyo.hd@gmail.com>
2020-03-15 14:50:47 +0900

    composer : v3d fkms not support RGBA8888

--- graphics/composer/2.1/default/drm_kms_vc4.cpp
+++ graphics/composer/2.1/default/drm_kms_vc4.cpp
@@ -52,7 +52,7 @@ static unsigned int drm_format_from_hal(int hal_format)
 		case HAL_PIXEL_FORMAT_RGBX_8888:
 			return DRM_FORMAT_XBGR8888;
 		case HAL_PIXEL_FORMAT_RGBA_8888:
-			return DRM_FORMAT_RGBA8888;
+			return DRM_FORMAT_ARGB8888;
 		case HAL_PIXEL_FORMAT_RGB_565:
 			return DRM_FORMAT_RGB565;
 		case HAL_PIXEL_FORMAT_YV12:
